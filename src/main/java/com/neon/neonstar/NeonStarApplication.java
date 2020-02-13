2ec2e6771da74282689baaf372672f3e61760b0c
Rama Ganapathy <ramu_ganapathy@yahoo.in>
2020-02-13 22:42:19 +0530

    add spring boot to the repo

---
+++ src/main/java/com/neon/neonstar/NeonStarApplication.java
@@ -0,0 +1,13 @@
+package com.neon.neonstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeonStarApplication.class, args);
+	}
+
+}
